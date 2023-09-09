1c29f6f0ba5c5fe4e614660641c69d41e6f16b54
Alex Bosch <abosch19@gmail.com>
2023-09-09 20:25:29 +0800

    feat: things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,24 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Svelte FAST Marquee',
-			social: {
-				github: 'https://github.com/abosch19/svelte-fast-marquee',
-			},
-			sidebar: [
-				{ label: 'Install', link: '/install/' },
-				{ label: 'Usage', link: '/usage/' },
-				{ label: 'Props', link: '/props/' }
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'Svelte FAST Marquee',
+    social: {
+      github: 'https://github.com/abosch19/svelte-fast-marquee'
+    },
+    sidebar: [{
+      label: 'Install',
+      link: '/install/'
+    }, {
+      label: 'Usage',
+      link: '/usage/'
+    }, {
+      label: 'Props',
+      link: '/props/'
+    }]
+  }), svelte()]
 });
